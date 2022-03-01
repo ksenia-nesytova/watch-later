976507ca7c368641ce61f1cf8c3d245a164e9d7b
ksenia nesytova <alleheldentot@gmail.com>
2022-03-01 22:04:53 +0700

    add placeholder movies data

---
+++ src/moviesData.js
@@ -0,0 +1,57 @@
+export default {
+    "success": true,
+    "data": {
+        "movies": [
+            {
+                "id": "0",
+                "name": "The Shawshank Redemption",
+            },
+            {
+                "id": "1",
+                "name": "Deadpool",
+            },
+            {
+                "id": "2",
+                "name": "Bohemian rhapsody",
+            },
+            {
+                "id": "3",
+                "name": "Lord of the rings trilogy",
+            },
+            {
+                "id": "4",
+                "name": "interstellar",
+            },
+            {
+                "id": "5",
+                "name": "Kingsman: the secret service",
+            },
+            {
+                "id": "6",
+                "name": "Seven",
+            },
+            {
+                "id": "7",
+                "name": "Law abiding citizen",
+            },
+            {
+                "id": "8",
+                "name": "Man on fire ",
+            },
+            {
+                "id": "9",
+                "name": "pride and prejudice",
+            },
+            {
+                "id": "10",
+                "name": " doctor sleep",
+            },
+            {
+                "id": "11",
+                "name": " the shining",
+            },
+
+
+        ]
+    }
+}
